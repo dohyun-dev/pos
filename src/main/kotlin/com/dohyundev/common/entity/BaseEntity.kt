79c89faa876722b01dfdcd026cbc2dddea09f284
권도현 <tobeykwon@gmail.com>
2025-10-27 22:17:07 +0900

    feat: 공통 엔티티 개선
    - BaseEntity에 DisplayOrderable 인터페이스 구현 추가
    - DisplayOrderable 인터페이스 추가 (순서 관리 기능)
    - TsidBaseEntity 제거 (BaseEntity로 통합)

--- src/main/kotlin/com/dohyundev/common/entity/BaseEntity.kt
+++ src/main/kotlin/com/dohyundev/common/entity/BaseEntity.kt
@@ -1,7 +1,8 @@
 package com.dohyundev.common.entity
 
-import jakarta.persistence.Column
+import io.hypersistence.utils.hibernate.id.Tsid
 import jakarta.persistence.EntityListeners
+import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
 import org.springframework.data.annotation.CreatedBy
 import org.springframework.data.annotation.CreatedDate
@@ -9,24 +10,25 @@ import org.springframework.data.annotation.LastModifiedBy
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.domain.AbstractAggregateRoot
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.Instant
 import java.time.LocalDateTime
 
 @EntityListeners(AuditingEntityListener::class)
 @MappedSuperclass
-abstract class BaseEntity<T : BaseEntity<T>> : AbstractAggregateRoot<T>() {
+abstract class BaseEntity : AbstractAggregateRoot<BaseEntity>() {
+    @Id
+    @Tsid
+    var id: Long? = null
+
     @CreatedDate
-    @Column(name = "created_at", updatable = false)
     var createdAt: LocalDateTime? = null
 
     @LastModifiedDate
-    @Column(name = "modified_at")
     var modifiedAt: LocalDateTime? = null
 
     @CreatedBy
-    @Column(name = "created_by", updatable = false)
-    var createdBy: String? = null
+    var createdBy: Long? = null
 
     @LastModifiedBy
-    @Column(name = "modified_by")
-    var modifiedBy: String? = null
+    var modifiedBy: Long? = null
 }
